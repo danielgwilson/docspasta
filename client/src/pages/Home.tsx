49ffb8a6357e3f1cb4ec7370825689499849f3da
Daniel G Wilson <daniel@danielgwilson.com>
2025-01-05 21:32:55 -0600

    Fix padding

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { CrawlerContainer } from '@/components/crawler/container';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-dvh bg-background flex flex-col justify-center items-center p-4">
       <CrawlerContainer />
     </div>
   );
