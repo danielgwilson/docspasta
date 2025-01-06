0044578904cd33d7ab674ced43750493d4a068c8
Daniel G Wilson <daniel@danielgwilson.com>
2025-01-05 21:53:15 -0600

    Add spline thing

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import { CrawlerContainer } from '@/components/crawler/container';
+import Spline from '@splinetool/react-spline';
 
 export default function Home() {
   return (
-    <div className="min-h-dvh bg-background flex flex-col justify-center items-center p-4">
-      <CrawlerContainer />
-    </div>
+    <main>
+      <div className="absolute inset-0 -z-10">
+        <Spline scene="https://prod.spline.design/dH29XAkRKntH2D-F/scene.splinecode" />
+      </div>
+      <div className="min-h-dvh flex flex-col justify-center items-center p-4">
+        <CrawlerContainer />
+      </div>
+    </main>
   );
 }
