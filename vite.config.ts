e4dbf67c1881de48a0e7707cb63aa37f74c7175e
Daniel G Wilson <daniel@danielgwilson.com>
2025-01-15 00:28:18 -0600

    Attempt to fix Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
-import path, { dirname } from "path";
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
+import react from '@vitejs/plugin-react';
+import themePlugin from '@replit/vite-plugin-shadcn-theme-json';
+import path, { dirname } from 'path';
 // import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,17 +17,18 @@ export default defineConfig({
   plugins: [
     react(),
     // runtimeErrorOverlay(),
-    themePlugin()
+    themePlugin(),
+    vercel(),
   ],
   resolve: {
     alias: {
-      "@db": path.resolve(__dirname, "db"),
-      "@": path.resolve(__dirname, "client", "src"),
+      '@db': path.resolve(__dirname, 'db'),
+      '@': path.resolve(__dirname, 'client', 'src'),
     },
   },
-  root: path.resolve(__dirname, "client"),
+  root: path.resolve(__dirname, 'client'),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, 'dist/public'),
     emptyOutDir: true,
   },
 });
